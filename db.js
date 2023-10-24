f71a0969588b77c3a534c6b902f7fe73d68e8b27
Sebastian Garcia <121516129+SebCodes27@users.noreply.github.com>
2023-10-24 12:13:31 -0700

    Full project completion

---
+++ db.js
@@ -0,0 +1,2 @@
+const mongoose = require('mongoose');
+mongoose.connect('mongodb+srv://sebastiangarciawork02:Shmongo42%2A@cluster0.p3lqwvg.mongodb.net/?retryWrites=true&w=majority', { useNewUrlParser: true, useUnifiedTopology: true });
--- index.js
+++ index.js
@@ -2,6 +2,31 @@ require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const app = express();
+let bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+mongoose.connect('mongodb+srv://sebastiangarciawork02:Shmongo42%2A@cluster0.p3lqwvg.mongodb.net/?retryWrites=true&w=majority', { useNewUrlParser: true, useUnifiedTopology: true });
+const dns = require('dns'); 
+
+// VISIT here for how to connect Mongoose https://dev.to/franciscomendes10866/setup-mongodb-with-mongoose-and-express-4c58
+
+// Mongoose code
+
+let UrlSchema = new mongoose.Schema({
+  originalUrl: {
+    type: String,
+    required: true
+  },
+  fixedUrl: {
+    type: String,
+    required: true
+  }
+});
+
+let Url = mongoose.model('Url', UrlSchema);
+
+let options = {
+  all: true,
+};
 
 // Basic Configuration
 const port = process.env.PORT || 3000;
@@ -19,15 +44,57 @@ app.get('/api/hello', function(req, res) {
   res.json({ greeting: 'hello API' });
 });
 
-app.post('/api/shorturl', (req, res) => {
-  let test = 'test';
+app.use('/api/shorturl', bodyParser.urlencoded({extended: false}));
+
+app.post('/api/shorturl', async (req, res) => {
   let originalURL = req.body.url;
-  let shortUrl = originalURL;
-  res.json({
-    original_url: originalURL,
-    short_url: test
+  Url.find()
+  .then((data) => {
+    let dataLength = data.length;
+    return dataLength;
   })
-})
+  .then((fixedURL) => {
+    let newUrl = new Url({ originalUrl: originalURL, fixedUrl: fixedURL });
+    console.log(newUrl);
+    return newUrl;
+  })
+  .then((savedUrl) => {
+    let saved = savedUrl.save();
+    return saved;
+  })
+  .then((data) => {
+    let replaceReg1 = /^https:?:\/\//i;
+    let replaceReg2 = /^http:?:\/\//i;
+    let replace1 = originalURL.replace(replaceReg1, '');
+    let replace2 = replace1.replace(replaceReg2, '');
+    dns.lookup(replace2, (err) => {
+      if (err && err.code === 'ENOTFOUND') {
+        res.json({
+          'error': 'invalid url'
+        })
+      } else {
+        res.json({
+          original_url: originalURL,
+          short_url: data.fixedUrl
+        })
+      }
+    })
+  })
+});
+
+app.get('/api/shorturl/:num?', async (req, res) => {
+  let num = Number(req.params.num)
+  Url.findOne({fixedUrl: num })
+  .then((docs)=>{
+      let redirect = docs.originalUrl;
+      res.redirect(redirect);
+      console.log('success');
+  })
+  .catch((err)=>{
+      res.send(err);
+      console.log('failure');
+  });
+});
 
 app.listen(port, function() {
   console.log(`Listening on port ${port}`);
